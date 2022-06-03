51be86453b3e465c4f1b8a0e53b0bc99af25dd1b
Syed Afrid Ali <aliatif908@gmail.com>
2022-06-04 01:51:16 +0530

    Added request logging and deployment build configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.use(morgan('combined'));
 app.use(planetsRouter);
 
 module.exports = app;
